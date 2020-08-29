b1fe8b25c31383fdb9aaae4fe373d6f6a0c2be76
Andrea Tchalaou <attchalaou@gmail.com>
2020-08-29 12:31:45 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,22 @@
+class Post 
+  
+  attr_accessor :title, :author
+  
+  @@all= []
+  
+  def initialize(title)
+    @title=title
+    @author=author
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def title 
+    @title 
+  end
+  
+  
+end
